029e13181bf043c92a9d12af9a04758d0a74b8a3
Alexander Torres Vivaldo <atorresv@andrew.cmu.edu>
2025-09-15 19:29:06 +0000

    Recommended username

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,10 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    // Make a username suggestion by adding suffix number
+                    const recommendation = username.concat(Math.random().toString(36).substring(2, 7)).concat('<br />');
+                    showError(username_notify, `[[error:username-taken]]. Try ${recommendation} instead.`);
+                    validationError = true;
                 }
 
                 callback();
