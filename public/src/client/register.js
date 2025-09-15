df53b512073ed5a55c1fdf9814387ad88ea5152f
Mandy1677 <mniu@andrew.cmu.edu>
2025-09-15 18:33:54 +0000

    changes made to register to suggest suffix

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,9 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    // showError(username_notify, '[[error:username-taken]]');
+                    const suggestion = username + 'suffix';
+                    showError(username_notify, `[[error:username-taken]] — try "${suggestion}" instead`);
                 }
 
                 callback();
